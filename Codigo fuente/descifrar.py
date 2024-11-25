522bedee22f35ca01b8d305daa8a2ebf8a210a3a
brenda rj <brendarj1124@gmail.com>
2024-11-24 23:49:55 -0600

    funciones interpolacion_lagrange y descifrar modificadas

--- Codigo fuente/descifrar.py
+++ Codigo fuente/descifrar.py
@@ -1,14 +1,13 @@
 def interpolacion_lagrange(valores_x, valores_y):
-    # Obtenemos el polinomio de Lagrange
-    n = len(valores_x)
+    r = len(valores_x)
     clave = 0
-    for i in range(n):
-        L = 1
-        for j in range(n):
+    for i in range(r):
+        polinomio_base = 1
+        for j in range(r):
             if i != j:
-                L = L * (valores_x[j]) / (valores_x[i] - valores_x[j])
-        clave = clave + L * valores_y[i]
-    return clave
+                polinomio_base *= (-valores_x[j])/(valores_x[i]-valores_x[j])
+        clave += polinomio_base * valores_y[i]
+    return int(clave)       
 
 def descifrar_AES(archivoAES, clave):
     # Desciframos el archivo con AES
@@ -17,19 +16,20 @@ def descifrar_AES(archivoAES, clave):
         archivo_descifrado = cipher.decrypt(file.read())
     with open(archivoAES, "wb") as file:
         file.write(archivo_descifrado)
-    
 
-def descifrar(archivoAES, archivoEvaluaciones):
-    # Extraemos las evaluaciones del archivo
-    with open(archivoEvaluaciones, "r") as file:
-        evaluaciones = file.readlines()
+def descifrar(archivoEvaluaciones, archivoAES):
     #Separamos las evaluaciones en 2 listas
     valores_x = []
     valores_y = []
-    for evaluacion in evaluaciones:
-        valores_x.append(evaluacion[0])
-        valores_y.append(evaluacion[1])
+    # Extraemos las evaluaciones del archivo
+    with open(archivoEvaluaciones, "r") as file:
+        for fila in file:
+            x, y = map(int, fila.strip().split())
+            valores_x.append(x)
+            valores_y.append(y)
     # Generamos la clave a partir de las evaluaciones
     clave = interpolacion_lagrange(valores_x, valores_y)
     # Desciframos el archivo con AES
     descifrar_AES(archivoAES, clave)
+
+    
